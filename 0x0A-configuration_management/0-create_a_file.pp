142c85168e03817ce63f61d95ed459e5b41dbb97
= <kareemwally@std.mans.edu.eg>
2024-03-22 23:53:39 +0000

    creatng puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
+# creates file using puppet
 file { '/tmp/school':
 	ensue => 'file',
-	owner =>'www-data',
 	group =>'www-data',
 	content =>'I love Puppet',
-	moed =>'0744',
+	mode =>'0744',
+	owner =>'www-data',
 	}
