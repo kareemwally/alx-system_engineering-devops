f8b8fe6453304f3f383f05d668331a653e0c44d1
= <kareemwally@std.mans.edu.eg>
2024-03-22 23:57:32 +0000

    creating file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # creates file using puppet
+
 file { '/tmp/school':
-	ensure => 'file',
-	group =>'www-data',
-	content =>'I love Puppet',
-	mode =>'0744',
-	owner =>'www-data',
-	}
+ ensure => 'file',
+ group => 'www-data',
+ content => 'I love Puppet',
+ mode => '0744',
+ owner => 'www-data',
+}
