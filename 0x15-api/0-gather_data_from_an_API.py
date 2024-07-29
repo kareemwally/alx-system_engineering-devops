7e2aa57716a3484f77300ffcab9ae883d1da6815
kareem <kareemwally@std.mans.edu.eg>
2024-07-29 12:56:24 +0300

    what

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,15 +2,17 @@
 """ sending requests to the test API https://jsonplaceholder.typicode.com/
     using requests module
 """
-import sys, requests
+import sys
+import requests
 
 user_id = sys.argv[1]
+
 
 def tasks_done(to_do):
     """ getting the number of achieved tasks"""
     res = 0
     for task in to_do:
-        if task["completed"] == True:
+        if task["completed"]:
             res += 1
     return res, len(to_do)
 
@@ -24,20 +26,20 @@ def snd_requests(link):
 def print_titles(to_do):
     """ printing titled of th achieved tasks"""
     for task in to_do:
-        if task["completed"] == True:
-            print("\t {}".format(task["title"]))
+        if task["completed"]:
+            print("\t {}".format(task.get("title")))
 
 
 res = snd_requests('https://jsonplaceholder.typicode.com/users/')
 
 user_name = res.json().get("name")
 
-req = requests.get('https://jsonplaceholder.typicode.com/todos', 
-        params={"userId":user_id})
+req = requests.get('https://jsonplaceholder.typicode.com/todos',
+                   params={"userId": user_id})
 
 to_do = req.json()
 print("Employee {0} is done with tasks({1}/{2}):"
-                    .format(user_name, 
-                        tasks_done(to_do)[0], tasks_done(to_do)[1]))
+      .format(user_name,
+              tasks_done(to_do)[0], tasks_done(to_do)[1]))
 
 print_titles(to_do)
