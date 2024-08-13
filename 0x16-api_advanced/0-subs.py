c31cff81872845206f7f2eaca249cf9c5325d672
kareem <kareemwally@std.mans.edu.eg>
2024-08-14 02:30:17 +0300

    no prob

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
+"""
+a simple function to get number of subs of subreddit
+"""
 import requests
+import sys
+
+client_id = "zg4h3mMKW60mJR1R7flA2Q"
+secret = "Svjdt2c-7UHBqZ-90I3XpKdvWfTcqA"
+auth = requests.auth.HTTPBasicAuth(client_id, secret)
+data = {
+        'grant_type': 'password',
+        'username': 'Queasy-Regret2317',
+        'password': ")%Fpdc$-!C%qE6A"
+        }
+headers = {'User-Agent': 'MyAPI'}
+res = requests.post("https://www.reddit.com/api/v1/access_token",
+                    auth=auth, data=data, headers=headers)
+token = res.json()['access_token']
+headers['Authorization'] = f'bearer {token}'
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of
-    subscribers for a given subreddit.
-    If an invalid subreddit is given, the function returns 0.
-    """
-    # Set the base URL and headers for the request
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent':
-               'Custom User Agent for querying subreddit subscribers'}
-
-    try:
-        # Send the GET request to the Reddit API
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        if response.status_code == 200:
-            data = response.json()
-            # Return the number of subscribers
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
-        # Handle any request exceptions and return 0
+    """ a function to startsending requests to API"""
+    test = requests.get('https://oauth.reddit.com/r/{}/about'
+                        .format(subreddit), headers=headers)
+    if test.status_code == 200:
+        return (test.json()["data"]["subscribers"])
+    else:
         return 0
